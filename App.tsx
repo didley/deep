744b59cc75e4d5726311ea1699afc0703188c4d0
Dylan Lamont <didley.dev@gmail.com>
2025-08-14 22:32:49 +1000

    init

---
+++ App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {
+  View,
+} from 'react-native';
+import { Text } from 'react-native-macos';
+
+const  App = () => {
+
+
+  return (
+    <View>
+<Text>Hello world</Text>
+    </View>
+  );
+}
+
+
+
+export default App;
---
+++ README.md
@@ -0,0 +1,15 @@
+# Getting Started
+
+## Install
+```
+npm i
+bundle install
+pod install
+```
+
+## Start
+
+```
+npm start
+npm start:macos
+```
