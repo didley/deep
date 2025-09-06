c01d29b3948a7948c8be5083971eeddb61c3a3dd
Dylan Lamont <didley.dev@gmail.com>
2025-09-06 23:13:47 +1000

    init terraform with AWS provider

---
+++ infra/aws/terraform.tf
@@ -0,0 +1,11 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.12.0"
+    }
+  }
+
+  required_version = ">= 1.13"
+}
+
---
+++ infra/aws/variables.tf
@@ -0,0 +1,10 @@
+variable "env" {
+  type        = string
+  description = "Environment for the resources"
+}
+
+variable "region" {
+  type        = string
+  description = "Base AWS region"
+}
+
